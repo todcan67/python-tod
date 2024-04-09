7ef48f8e547657581f797c162140d21c763c0dd1
todcan67 <toddtwining@gmail.com>
2024-04-09 08:54:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# python-tod
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/todcan67/python-tod)
---
+++ nodupes.py
@@ -0,0 +1,9 @@
+# Run `python nodupes.py` in the terminal
+
+# Note: Python is lazy loaded so the first run will take a moment,
+# But after cached, subsequent loads are super fast! ⚡️
+
+
+list = {'this', 'that', 'this',}
+print(list)
+
